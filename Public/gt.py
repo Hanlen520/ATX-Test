c78ede1fc30fdb79a400d13c37f2acaec66c3276
pengchenglin <m170587788@163.com>
2018-04-24 20:56:37 +0800

    zip report folder

--- Public/gt.py
+++ Public/gt.py
@@ -9,6 +9,8 @@
 import functools
 from logzero import logger
 import time
+import zipfile
+import os
 
 
 class GT(object):
@@ -105,14 +107,16 @@ class GT(object):
         self.d.pull('/sdcard/GTRData/data.js', dst)
         logger.info('Pull data.js success')
 
+
 def zip_report(name):
     startdir = "../GT_Report"  # 要压缩的文件夹路径
-    file_news = name + '.zip'  # 压缩后文件夹的名字
+    file_news = '../'+ name + '.zip'  # 压缩后文件夹的名字
     z = zipfile.ZipFile(file_news, 'w', zipfile.ZIP_DEFLATED)  # 参数一：文件夹名
     for dirpath, dirnames, filenames in os.walk(startdir):
-        fpath = dirpath.replace(startdir, '')  # 这一句很重要，不replace的话，就从根目录开始复制
-        fpath = fpath and fpath + os.sep or ''  # 这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
+        # fpath = dirpath.replace(startdir, '')  # 这一句很重要，不replace的话，就从根目录开始复制
+        # fpath = fpath and fpath + os.sep or ''  # 这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
         for filename in filenames:
-            z.write(os.path.join(dirpath, filename), fpath + filename)
-            print('压缩成功')
+            # z.write(os.path.join(dirpath, filename), fpath + filename)
+            z.write(os.path.join(dirpath, filename))
     z.close()
+    logger.info('zip report folder success')
