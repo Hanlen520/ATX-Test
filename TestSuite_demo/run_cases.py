61acfa5b16ed0931eb46dd6c56cbef2871b753da
linpengcheng <pengcheng.lin@quvideo.com>
2018-06-08 16:31:19 +0800

    update

--- TestSuite_demo/run_cases.py
+++ TestSuite_demo/run_cases.py
@@ -4,7 +4,6 @@ import sys
 # sys.path.append(os.path.split(os.path.split(os.path.abspath(''))[0])[0])
 sys.path.append('..')
 from Public.CaseStrategy import CaseStrategy
-
 from Public.Drivers import Drivers
 
 
--- run_all_cases.py
+++ run_all_cases.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append('..')
+sys.path.append('.')
 from Public.CaseStrategy import CaseStrategy
 from Public.Drivers import Drivers
 
@@ -8,6 +8,4 @@ from Public.Drivers import Drivers
 if __name__ == '__main__':
     cs = CaseStrategy()
     cases = cs.collect_cases(suite=True)
-
-    # in future, cases_list may be used for testing strategy in multi devices
     Drivers().run(cases)
